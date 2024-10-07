264cac4014791a6b1c61d00ca8bb5d2a30e8a96a
juancarlosfc5 <181221774+juancarlosfc5@users.noreply.github.com>
2024-10-06 20:14:09 -0500

    V5 - 1 vs IA con escudo

--- modules/games.py
+++ modules/games.py
@@ -8,14 +8,10 @@ import modules.players as pl
 opciones = ['piedra', 'papel', 'tijera']
 
 # Jugador vs IA
-contadorJugador = 0
-contadorIA = 0
-escudoJugador = False
-escudoIA = False
-
 
 def rondaIA(origin):
-    """Realiza una ronda de piedra, papel o tijera"""
+    ut.borrar()
+    print("Inicia la ronda\n")
     eleccionIA = random.choice(opciones) # La IA elige una opción al azar
     eleccionJugador = input("Escribe tu elección piedra, papel o tijera: ").lower() # El jugador elige su jugada
     while eleccionJugador not in opciones:
@@ -34,6 +30,10 @@ def rondaIA(origin):
 def juegoIA(origin):
     rondasJugador = 0
     rondasIA = 0
+    contadorJugador = 0
+    contadorIA = 0
+    escudoJugador = False
+    escudoIA = False
     isJuegoIA = True
     while isJuegoIA:
         ut.borrar()
@@ -45,24 +45,48 @@ def juegoIA(origin):
         else:
             ut.borrar()
             print(f'Bienvenido {nickname} inicia el juego vs la IA\n')
+            ut.pausar()
             while rondasJugador < 3 and rondasIA < 3: # Criterio mejor de 3 rondas
                 resultado = rondaIA(origin)
-                if resultado == 'usuario':
-                    rondasJugador += 1
-                    print(f"\n¡Ganaste esta ronda! --- Usuario: {rondasJugador} - IA {rondasIA}\n")
-                elif resultado == 'ia':
-                    rondasIA += 1
-                    print(f"\n¡La IA ganó esta ronda! --- Usuario: {rondasJugador} - IA {rondasIA}\n")
-                elif resultado == 'empate':
-                    print(f"\n¡Es un empate! --- Usuario: {rondasJugador} - IA {rondasIA}\n")
+                if resultado == 'usuario': # Incrementar victorias consecutivas del jugador
+                    contadorJugador += 1
+                    contadorIA = 0
+                    if contadorJugador == 2: # Asignar escudo al jugador
+                        escudoJugador = True
+                        print("\n¡Has ganado dos rondas consecutivas! Escudo activado.")
+                    if escudoIA == True: # Verificar escudo de la IA
+                        escudoIA = False
+                        print("\n¡La IA perdió, pero su escudo la protegió!")
+                    else: # Incrementar ronda del jugador
+                        rondasJugador += 1
+                        print(f"\n¡Ganaste esta ronda! --- Usuario: {rondasJugador} - IA {rondasIA}\n")
+                        ut.pausar()
+                elif resultado == 'ia': # Incrementar victorias consecutivas de la IA
+                    contadorIA += 1
+                    contadorJugador = 0
+                    if contadorIA == 2: # Asignar escudo a la IA
+                        escudoIA = True
+                        print("\n¡La IA ha ganado dos rondas consecutivas! Escudo activado.")
+                    if escudoJugador == True: # Verificar escudo del jugador
+                        escudoJugador = False
+                        print("\n¡Has perdido, pero tu escudo te protegiós!")
+                    else: # Incrementar ronda de la IA
+                        rondasIA += 1
+                        print(f"\n¡La IA ganó esta ronda! --- Usuario: {rondasJugador} - IA {rondasIA}\n")
+                        ut.pausar()
+                elif resultado == 'empate': # Validar empate
+                    print(f"\n¡Empate! --- Usuario: {rondasJugador} - IA {rondasIA}\n")
+                    ut.pausar()
 
             if rondasJugador == 3:
-                origin[nickname]['winIA'] += 1  # Incrementar victorias del jugador
-                print("\n¡Felicitaciones! Has ganado la partida.")
+                origin[nickname]['winIA'] += 1  # Asignar victoria del jugador ante la IA
+                ut.borrar()
+                print("¡Felicitaciones! Has ganado la partida.\n")
                 ut.pausar()
                 isJuegoIA = False
             else:
-                origin[nickname]['loseIA'] += 1  # Incrementar derrotas del jugador
-                print("\nLa IA ha ganado la partida.")
+                origin[nickname]['loseIA'] += 1  # Asignar derrota del jugador ante la IA
+                ut.borrar()
+                print("¡Lo siento! La IA ha ganado la partida.\n")
                 ut.pausar()
                 isJuegoIA = False
